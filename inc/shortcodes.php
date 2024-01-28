bd545c5b495b74c1c50065d6b0484f5278a87694
Michael Anderau <webstuff@sternenaugen.net>
2024-01-28 12:38:18 +0100

    add agenda-mini shortcode

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -197,7 +197,7 @@ function sd_widget_agenda( $atts ) {
                                 }
                                 // single day event
                                 elseif( wp_date('Y-m-d', $date_begin) == wp_date('Y-m-d', $date_end) ){
-                                  echo '<div>' . ucfirst(wp_date( 'D, d. F', $date_end )) . '</div>, <div>' . wp_date( $format_begin_time, $date_begin ) . ' – ' . wp_date( $format_end_time, $date_end ) . '/div>';
+                                  echo '<div>' . ucfirst(wp_date( 'D, d. F', $date_end )) . '</div>, <div>' . wp_date( $format_begin_time, $date_begin ) . ' – ' . wp_date( $format_end_time, $date_end ) . '</div>';
                                 }
                                 else {
 
@@ -257,4 +257,170 @@ function sd_widget_agenda( $atts ) {
  * register custom shortcodes in WordPress
  */
 add_shortcode( 'sd-widget-agenda', 'sd_widget_agenda' );
-?>
+
+
+
+/** ###############################################
+ * shortcode sd-widget-agenda-mini
+ ##################################################  */
+
+function sd_widget_agenda_mini( $atts ) {
+	$atts = shortcode_atts ([
+      'category' => 0, // Filter by category (SeminarDesk Label ID)? integer, 0 (=show all)
+      'limit' => 0, // Limit number of items in list? integer, default: 0 (=no limit)
+	], $atts);
+  $category = $atts['category'] ?: $_GET['category'] ?? 0;
+  
+	// enable output buffering
+	ob_start();
+
+	/**
+	 * BEGIN of template code ...
+	 */
+
+	?>
+	<div id="shortcode-agenda-mini">
+		<div id="widget-upcoming">
+			<section class="splide" aria-labelledby="upcoming-heading">
+				<div class="splide__track">
+					<div class="splide__list">
+						<?php
+            // Get dates
+            $dates_upcoming = ivy_get_dates_upcoming_all();
+            
+            $i = 0;
+            $dates_filtered = $dates_upcoming;
+            foreach ( $dates_upcoming as $date ){
+              if ( 
+                $date->sd_data['bookingPageStatus'] === 'hidden_on_list' || 
+                $date->sd_data['bookingPageStatus'] === 'hidden' || 
+                empty($date->sd_preview_available) || 
+                $category && !sd_date_has_category($date, $category)
+
+                ) {
+                  // $is_remove = true;
+                  unset ( $dates_filtered[$i] );
+							}
+							$i++;
+						}
+						$dates_upcoming = $dates_filtered;
+            $count = 0;
+            
+						foreach( $dates_upcoming as $date){
+              $wp_timestamp_today = strtotime(wp_date('Y-m-d'));
+  						if (
+                (
+                  // Category filter?
+                  !$category || 
+                  sd_date_has_category($date, $category)
+                ) &&
+                (
+                  // Date filter: Don't show past dates (date_begin < today)
+                  $wp_timestamp_today <= $date->sd_date_begin/1000
+                ) && 
+                (
+                  // Limit items?
+                  ($atts['limit'] == 0 || $count < $atts['limit'])
+                )
+              ) {
+                $count++;
+                
+                $event = get_post( $date->wp_event_id );
+                
+                // Get title and subtitles
+                $event_title = $event->sd_data['title'][0]['value'];
+                $event_subtitle = $event->sd_data['subtitle'][0]['value'];
+                
+                // Get venue
+                $venue = $date->sd_data['venue'];
+                $venue_address = implode(', ', array_filter([
+                  $venue['name'], 
+//                  $venue['street1'], 
+//                  $venue['street2'], 
+//                  $venue['zip'], 
+                  $venue['city']
+                ]));
+                
+                // Get status
+                $booking_status = $date->sd_data['bookingPageStatus'];
+                $booking_status_label = IVY_STRINGS['status'][$booking_status]??ucwords($booking_status);
+                ?>
+                <div class="sd-event">
+                  <a class="box" href="<?php echo get_permalink( $date->wp_event_id ); ?>">
+                  
+                <div class="content">
+                  
+                    <div class="sd-event-date ">
+                    <?php
+                                //date start to end
+                                $date_begin = $date->sd_date_begin/1000;
+                                $date_end = $date->sd_date_end/1000;
+                                    $format_begin_time = wp_date( 'i', $date_begin ) != '00' ? 'G\hi' : 'G\h';
+                                    $format_end_time = wp_date( 'i', $date_end ) != '00' ? 'G\hi' : 'G\h';
+                                // single day event of today
+                                  if( wp_date('Y-m-d', $date_begin) == wp_date('Y-m-d', $date_end) && wp_date('Y-m-d') == wp_date('Y-m-d', $date_end) ){
+                                  echo '<span>' . ucfirst( IVY_STRINGS['today'] ) . '</span>';
+                                  $format_begin = wp_date( 'i', $date_begin ) != '00' ? 'G\hi' : 'G\h';
+                                  echo ', <span>' . wp_date( $format_begin_time, $date_begin ) . ' – ' . wp_date( $format_end_time, $date_end ) . '</span>';
+                                }
+                                // begins today
+                                elseif( wp_date('Y-m-d') == wp_date('Y-m-d', $date_begin) ){
+                                  echo '<span>' . ucfirst( IVY_STRINGS['today'] ) . '</span> – <span>' . ucfirst(wp_date( 'D, d. F', $date_end )) . '</span>';
+                                }
+                                 // ends today
+                                elseif( wp_date('Y-m-d') == wp_date('Y-m-d', $date_end) ){
+                                  echo '<span>' . ucfirst(wp_date( 'd.m.', $date_begin )) . '</span> – <span>' . ucfirst( IVY_STRINGS['today'] ) . '</span>';
+                                }
+                                // single day event
+                                elseif( wp_date('Y-m-d', $date_begin) == wp_date('Y-m-d', $date_end) ){
+                                  echo '<span>' . ucfirst(wp_date( 'd.m.', $date_end )) . '</span>, <span>' . wp_date( $format_begin_time, $date_begin ) . ' – ' . wp_date( $format_end_time, $date_end ) . '</span>';
+                                }
+                                else {
+
+                                  if( wp_date('Y', $date_begin) !== wp_date('Y', $date_end) ) {
+                                    echo '<span>' . ucfirst(wp_date( 'd.m.Y', $date_begin )) . '</span> – <span>' . ucfirst(wp_date( 'D, d. F Y', $date_end )) . '</span>';
+                                  }
+                                  elseif( wp_date('m', $date_begin) !== wp_date('m', $date_end) && wp_date('Y', $date_begin) == wp_date('Y', $date_end) ) {
+                                    echo '<span>' . ucfirst(wp_date( 'd.m.', $date_begin )) . '</span> – <span>' . ucfirst(wp_date( 'D, d. F Y', $date_end )) . '</span>';
+                                  }
+                                  else {
+                                    echo '<span>' . ucfirst(wp_date( 'd.', $date_begin )) . '</span> – <span>' . ucfirst(wp_date( 'd.m.Y', $date_end )) . '</span>';
+                                  }
+                                }
+                                ?>
+                    <br />
+                    <span class="title"><strong><?= wp_strip_all_tags($event_title); ?></strong></span>
+                    <?php if ($event_subtitle && $event_title !== $event_subtitle) : ?>
+                 	  <span class="subtitle"> - <?= wp_strip_all_tags($event_subtitle); ?></span>
+                    <?php endif; ?>
+                    <br />
+                    <?php if ($venue_address) : ?>
+                      <span><?= $venue_address ?></span>
+                    <?php endif; ?>
+                </div>
+                </div>
+                </a>
+                <?php
+              }
+            }
+					?>
+			</section>
+		</div>
+	</div>
+
+	<?php
+	/**
+	 * End of template code ...
+	 */
+
+	// disable output buffer
+	$return = ob_get_contents();
+	ob_end_clean();
+
+	return $return;
+}
+
+/**
+ * register custom shortcodes in WordPress
+ */
+add_shortcode( 'sd-widget-agenda-mini', 'sd_widget_agenda_mini' );
