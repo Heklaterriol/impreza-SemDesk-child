b32bdb9ab514153a997281f0f5d1435cc85a4d25
Benno Flory <benno.flory@gmx.ch>
2024-02-01 00:37:43 +0100

    Support multiple booking buttons on a single page

--- seminardesk-custom/templates/assets/sd_cpt_event.js
+++ seminardesk-custom/templates/assets/sd_cpt_event.js
@@ -6,9 +6,9 @@
 
 // get the modal and its content
 var modal = document.querySelector(".sd-modal");
-var btnBooking = document.querySelector(".sd-modal-booking-btn");
+var btnBooking = document.querySelectorAll(".sd-modal-booking-btn");
 // get button that closes the modal
-var btnClose = document.querySelector(".sd-modal-close-btn");
+var btnClose = document.querySelectorAll(".sd-modal-close-btn");
 
 // toggle between show and hide of the modal
 function sdModalToggle() {
@@ -22,8 +22,7 @@ function windowOnClick(event) {
 
 // click events listener
 if ( btnBooking !== null && modal !== null ){
-	btnBooking.addEventListener("click", sdModalToggle);
-	btnClose.addEventListener("click", sdModalToggle);
+	btnBooking.forEach(function(e){ e.addEventListener("click", sdModalToggle); });
+	btnClose.forEach(function(e){ e.addEventListener("click", sdModalToggle); });
 	window.addEventListener("click", windowOnClick);
 }
-
